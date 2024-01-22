c1ecedf31eb61e9e359473502f3fac40d0836f1a
Trungdoan2009 <156778915+Trungdoan2009@users.noreply.github.com>
2024-01-22 21:40:21 +0700

    views.py

--- tools/views.py
+++ tools/views.py
@@ -10,7 +10,7 @@ import requests
 #Asssign your API key below
 #Get it from here:
 #https://platform.openai.com/api-keys
-apiKey = "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"
+apiKey = "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"
 
 
 def chat(request):
